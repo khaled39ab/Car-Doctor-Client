e4fc465278ed67d2137937f53b4c6e03accbcd8b
khaled39ab <khaled39ab@gmail.com>
2023-02-11 21:39:32 +0600

    install dependencies

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-secondary">Button</button>
     </div>
   );
 }
