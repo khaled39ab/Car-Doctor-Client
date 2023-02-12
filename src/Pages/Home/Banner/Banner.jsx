e787f51b84896115d0d5a6ccc4c57f45e0ccf0b9
khaled39ab <khaled39ab@gmail.com>
2023-02-12 16:39:57 +0600

    customized carousel banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Banner.css';
 import banner1 from '../../../assets/images/banner/1.jpg';
 import banner2 from '../../../assets/images/banner/2.jpg';
 import banner3 from '../../../assets/images/banner/3.jpg';
@@ -9,12 +10,28 @@ import banner6 from '../../../assets/images/banner/6.jpg';
 const Banner = () => {
 
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full ">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={banner1} alt='' className="w-full h-5/6" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+                <div className='carousel-img'>
+                    <img src={banner1} alt='' className="w-full" />
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 left-20 top-1/4">
+                    <h1 className='text-5xl font-bold text-white'>
+                        Affordable <br />
+                        Price For Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 left-20 top-1/2 w-2/5">
+                    <p className=' text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 left-20 top-2/3 w-2/5">
+                    <button className="btn btn-warning mr-5">Discover More</button>
+                    <button className="btn btn-outline btn-warning">Latest work</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-red-500">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        '80vh': '80vh',
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
