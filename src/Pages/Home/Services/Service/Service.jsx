957e7f850aadb72bea878f179df9a94bc2510eb5
khaled39ab <khaled39ab@gmail.com>
2023-02-13 08:28:48 +0600

    services page done

--- src/Pages/Home/Services/Service/Service.jsx
+++ src/Pages/Home/Services/Service/Service.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Service = () => {
-    
+const Service = ({service}) => {
+    const {title, img, price} = service;
+    return (
+        <div className="card  w-72 h-full bg-gray-200 shadow-xl">
+            <figure><img className='w-11/12 mt-3' src={img} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {title}
+                </h2>
+                <div className="card-actions justify-between">
+                    <p className="text-orange-600">Price: ${price}</p>
+                    <div className="badge badge-outline">❯</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Service;
--- src/Pages/Home/Services/Services/Services.jsx
+++ src/Pages/Home/Services/Services/Services.jsx
@@ -11,19 +11,22 @@ const Services = () => {
     }, []);
 
     return (
-        <div className="text-center py-10 bg-neutral-100">
-            <div>
+        <div className="py-10 bg-neutral-100">
+            <div className='text-center'>
                 <p className="text-xl font-bold text-orange-600">Services</p>
                 <h1 className='text-5xl py-5 text-black'>Our Service Area</h1>
                 <p className='w-3/5 mx-auto'>The majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable. </p>
             </div>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-5 mx-auto'>
                 {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
                     ></Service>)
                 }
+            </div>
+            <div className='text-center my-10'>
+                <button className="btn btn-outline btn-secondary">More Services</button>
             </div>
         </div>
     );
