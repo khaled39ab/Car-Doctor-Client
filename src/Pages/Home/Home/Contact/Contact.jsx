6f60814cdff129060f46bdc301336cfce62d6a06
khaled39ab <khaled39ab@gmail.com>
2023-02-13 21:08:53 +0600

    creating contact page

--- src/Pages/Home/Home/Contact/Contact.jsx
+++ src/Pages/Home/Home/Contact/Contact.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className="bg-black text-white flex justify-evenly p-20 rounded-lg">
+            <div className="flex">
+                <div className="">
+
+                </div>
+                <div className="">
+                    <p className="text-sm">We are open monday-friday</p>
+                    <p className="text-2xl">7:00 am - 9:00 pm</p>
+                </div>
+            </div>
+            <div className="flex">
+                <div className="">
+
+                </div>
+                <div className="">
+                    <p className="text-sm">Have a question?</p>
+                    <p className="text-2xl">+2546 251 2658</p>
+                </div>
+            </div>
+            <div className="flex">
+                <div className="">
+
+                </div>
+                <div className="">
+                    <p className="text-sm">Need a repair? our address</p>
+                    <p className="text-2xl">Liza Street, New York</p>
+                </div>
+            </div>
         </div>
     );
 };
