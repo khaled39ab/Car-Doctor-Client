2b30c7151f48b44c5f5054b127b01f4d4832b411
khaled39ab <khaled39ab@gmail.com>
2023-02-14 16:36:25 +0600

    running testimonial section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Banner from '../Banner/Banner/Banner';
 import Services from '../Services/Services/Services';
 import Contact from '../Contact/Contact';
 import Products from '../Products/Products/Products';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services />
             <Contact />
             <Products />
+            <Testimonial />
         </div>
     );
 };
---
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className="text-xl font-bold text-orange-600">Testimonial</p>
+                <h1 className='text-5xl py-5 text-black'>What Customer Says</h1>
+                <p className='w-3/5 mx-auto'>The majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable. </p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Testimonial;
