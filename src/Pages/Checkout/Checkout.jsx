7fc00c3fb714030edf82173a5e0472cf19532870
khaled39ab <khaled39ab@gmail.com>
2023-02-18 22:10:24 +0600

    created checkout page

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -7,6 +7,16 @@ const Checkout = () => {
             <div>
                 <ServiceBanner pageName={"Checkout"} />
             </div>
+            <form className='bg-base-200 py-14 px-10 rounded-md'>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-5 '>
+                    <input type="text" placeholder="Your Name" className="input input-bordered w-full" />
+                    <input type="text" placeholder="Your Email" className="input input-bordered w-full" />
+                    <input type="text" placeholder="Phone Number" className="input input-bordered w-full" />
+                    <input type="text" placeholder="Your Address" className="input input-bordered w-full" />
+                </div>
+                <textarea placeholder="Message" className="textarea textarea-bordered textarea-lg w-full m-5" ></textarea>
+                <button className="btn btn-block mt-7 bg-[#ff3811]">Order Confirm</button>
+            </form>
         </div>
     );
 };
