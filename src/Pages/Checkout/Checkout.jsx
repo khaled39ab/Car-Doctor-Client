023b205be8a5afa360475fd1b67485510e411044
khaled39ab <khaled39ab@gmail.com>
2023-02-18 22:27:38 +0600

    some customized

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -3,20 +3,20 @@ import ServiceBanner from '../Shared/ServiceBanner/ServiceBanner';
 
 const Checkout = () => {
     return (
-        <div className='p-8'>
-            <div>
-                <ServiceBanner pageName={"Checkout"} />
+        <div className='px-8 my-5'>
+            <ServiceBanner pageName={"Checkout"} />
+            <div className='my-5'>
+                <form className='bg-base-200 py-14 px-10 rounded-md'>
+                    <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-5 '>
+                        <input type="text" placeholder="Your Name" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Your Email" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Phone Number" className="input input-bordered w-full" />
+                        <input type="text" placeholder="Your Address" className="input input-bordered w-full" />
+                    </div>
+                    <textarea placeholder="Message" className="textarea textarea-bordered textarea-lg w-full mt-5" ></textarea>
+                    <button className="btn btn-block mt-7 bg-[#ff3811]">Order Confirm</button>
+                </form>
             </div>
-            <form className='bg-base-200 py-14 px-10 rounded-md'>
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-5 '>
-                    <input type="text" placeholder="Your Name" className="input input-bordered w-full" />
-                    <input type="text" placeholder="Your Email" className="input input-bordered w-full" />
-                    <input type="text" placeholder="Phone Number" className="input input-bordered w-full" />
-                    <input type="text" placeholder="Your Address" className="input input-bordered w-full" />
-                </div>
-                <textarea placeholder="Message" className="textarea textarea-bordered textarea-lg w-full m-5" ></textarea>
-                <button className="btn btn-block mt-7 bg-[#ff3811]">Order Confirm</button>
-            </form>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -7,19 +7,19 @@ const Footer = () => {
         <footer className="footer p-28 bg-slate-700 text-white">
             <div>
                 <img className='bg-white' src={logo} alt="" />
-                <p>Motor Service Co.<br />Providing reliable Motor since 2020</p>
+                <p>Car Doctor (Servicing) Co.<br />Providing reliable service since 2020</p>
             </div>
             <div>
                 <span className="footer-title">Services</span>
-                <Link to={'/'} className="link link-hover">Branding</Link>
-                <Link to={'/'} className="link link-hover">Design</Link>
+                <Link to={'/services'} className="link link-hover">Services</Link>
+                <Link to={'/products'} className="link link-hover">Products</Link>
                 <Link to={'/'} className="link link-hover">Marketing</Link>
                 <Link to={'/'} className="link link-hover">Advertisement</Link>
             </div>
             <div>
                 <span className="footer-title">Company</span>
-                <Link to={'/'} className="link link-hover">About us</Link>
-                <Link to={'/'} className="link link-hover">Contact</Link>
+                <Link to={'/about'} className="link link-hover">About us</Link>
+                <Link to={'/contact'} className="link link-hover">Contact</Link>
                 <Link to={'/'} className="link link-hover">Jobs</Link>
                 <Link to={'/'} className="link link-hover">Press kit</Link>
             </div>
