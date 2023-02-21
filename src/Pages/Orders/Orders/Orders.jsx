66e913ee68077c9dd71ccdc484d09b0d2343abbe
khaled39ab <khaled39ab@gmail.com>
2023-02-22 00:32:24 +0600

    delete code of handleAllDelete

--- src/Pages/Orders/Orders/Orders.jsx
+++ src/Pages/Orders/Orders/Orders.jsx
@@ -4,43 +4,27 @@ import { OrderListContext } from '../../../Context/OrderContext/OrderContext';
 import Order from '../Order/Order';
 
 const Orders = () => {
-    const { orders,setOrders } = useContext(OrderListContext);
+    const { orders, setOrders } = useContext(OrderListContext);
 
-    
+
     const handleDelete = id => {
         const proceed = window.confirm('Are you sure to cancel this order');
 
-        if(proceed){
-            fetch(`http://localhost:4000/orders/${id}`,{
+        if (proceed) {
+            fetch(`http://localhost:4000/orders/${id}`, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data => {
-                if (data.deletedCount > 0){
-                    toast("Order Cancel Successfully")
-                    const remaining = orders.filter(odr => odr._id !== id)
-                    setOrders(remaining)
-                }
-            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        toast("Order Cancel Successfully")
+                        const remaining = orders.filter(odr => odr._id !== id)
+                        setOrders(remaining)
+                    }
+                })
         }
     };
 
-
-    const handleAllDelete = () => {
-        const proceed = window.confirm('Are you sure to cancel all order');
-
-        if(proceed){
-            fetch(`http://localhost:4000/orders/`,{
-                method: 'DELETE'
-            })
-            .then(res => res.json())
-            .then(data => {
-                if (data.deletedCount > 0){
-                    toast("Cancel all Order Successfully")
-                }
-            })
-        }
-    }
 
     return (
         <div className="overflow-x-auto w-full my-10">
@@ -51,7 +35,7 @@ const Orders = () => {
                     <tr>
                         <th>
                             <label>
-                                <button onClick={handleAllDelete} className="btn btn-circle btn-outline">
+                                <button className="btn btn-circle btn-outline">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                 </button>
                             </label>
