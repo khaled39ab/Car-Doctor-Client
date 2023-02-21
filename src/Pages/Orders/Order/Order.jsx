17a9144e8ce83f092277f3816412aadb54a39bbb
khaled39ab <khaled39ab@gmail.com>
2023-02-21 23:42:00 +0600

    handle delete an order

--- src/Pages/Orders/Order/Order.jsx
+++ src/Pages/Orders/Order/Order.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
 const Order = ({ order }) => {
-    const { serviceId, name, service, branch, price, phone, status } = order; //imagine value status
+    const { _id, serviceId, name, service, branch, price, phone, status } = order; //imagine value status
     const [orderService, setOrderService] = useState({});
+    const { img } = orderService;
 
     useEffect(() => {
         fetch(`http://localhost:4000/service/${serviceId}`)
             .then(res => res.json())
             .then(data => setOrderService(data))
-    }, [serviceId])
+    }, [serviceId]);
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure to cancel this order');
+
+        if(proceed){
+            fetch(`http://localhost:4000/orders/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+        }
+    }
 
     return (
         <tbody>
             <tr className="hover">
                 <th>
                     <label>
-                        <button className="btn btn-circle btn-outline">
+                        <button className="btn btn-circle btn-outline" onClick={() =>handleDelete(_id)}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                         </button>
                     </label>
@@ -24,7 +37,7 @@ const Order = ({ order }) => {
                     <div className="flex items-center space-x-3">
                         <div className="avatar">
                             <div className="rounded w-20 h-16">
-                                <img src={orderService.img} alt="services " />
+                                <img src={img} alt="services " />
                             </div>
                         </div>
                         <div>
@@ -33,8 +46,8 @@ const Order = ({ order }) => {
                         </div>
                     </div>
                 </td>
-                <td className='font-semibold'>
-                    {name}
+                <td >
+                    <span className='font-bold'>{name}</span>
                     <br />
                     <span className="badge badge-ghost badge-md">{phone}</span>
                 </td>
